a9078bb7486c111336fb9b86e3832d28de3c6e01
ramses2099 <ramses2099@gmail.com>
2024-09-06 11:19:09 -0400

    first data frame

---
+++ script001.R
@@ -0,0 +1,7 @@
+# firt data frame
+p.height <- c(180, 155, 160, 167, 181)
+p.weight <- c(65, 50, 52, 58, 70)
+p.names <- c("Joanna", "Charlotte", "Helen", "Karen", "Amy")
+
+df <- data.frame(height = p.height, weight= p.weight, names = p.names)
+df
